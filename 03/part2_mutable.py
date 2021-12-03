4c151da653759d714ced3b3fdd705c1d71506dbd
Kevin Coffey <kevin.coffey@datadoghq.com>
2021-12-03 17:26:24 -0500

    Kill old code

--- 03/part2_mutable.py
+++ 03/part2_mutable.py
@@ -1,79 +1,8 @@
-from functools import reduce
 import os
 from contextlib import contextmanager
-from typing import Iterable, Iterator, NamedTuple, Optional, Callable
+from typing import Iterable, Iterator, Optional, Callable
 from dataclasses import dataclass
 import operator
-
-
-class TreeFunctional(NamedTuple):
-
-    zero: Optional["TreeFunctional"] = None
-    one: Optional["TreeFunctional"] = None
-
-    size: int = 0
-    value: str = ""
-
-    def add_word(self, word: str) -> "TreeFunctional":
-        return self._add_word_functional(word)
-
-    def _add_word_functional(self, word: str, index: int = 0) -> "TreeFunctional":
-        if index >= len(word):
-            # finish them
-            return TreeFunctional(
-                one=self.one, zero=self.zero, value=word, size=self.size + 1
-            )
-        else:
-            l = word[index]
-            if l == "0":
-                zero = self.zero or TreeFunctional()
-                return TreeFunctional(
-                    one=self.one,
-                    zero=zero._add_word_functional(word, index + 1),
-                    size=self.size + 1,
-                )
-            else:
-                one = self.one or TreeFunctional()
-                return TreeFunctional(
-                    one=one._add_word_functional(word, index + 1),
-                    zero=self.zero,
-                    size=self.size + 1,
-                )
-
-    def oxygen(self) -> str:
-        """
-        To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position,
-        and keep only numbers with that bit in that position.
-        If 0 and 1 are equally common, keep values with a 1 in the position being considered.
-        """
-        return self._walk(operator.ge)
-
-    def co2(self) -> str:
-        """
-        To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position,
-        and keep only numbers with that bit in that position.
-        If 0 and 1 are equally common, keep values with a 0 in the position being considered.
-        """
-        return self._walk(operator.lt)
-
-    def _walk(self, comparison: Callable[..., bool]) -> str:
-        # Traverse down the tree
-        if self.zero is not None and self.one is not None:
-            # Two good children, compare sizes
-            if comparison(self.one.size, self.zero.size):
-                node = self.one
-            else:
-                node = self.zero
-        elif self.one is not None:
-            # Single child
-            node = self.one
-        elif self.zero is not None:
-            # Single child
-            node = self.zero
-        else:
-            # Found a Leaf
-            return self.value
-        return node._walk(comparison)
 
 
 @dataclass
